774175ad457718fb9238a68b68b9388f26778167
Zhambyl <73802128+jamber751@users.noreply.github.com>
2025-05-02 00:48:44 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,6 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
-app.include_router(company.router)
 app.include_router(chatgpt_api.router)
 app.include_router(create_post.router)
 app.include_router(company.router)
